4f4458eaddb13550aa33e23ae50d2ca433d39f84
Mehul Lakhanpal <318097@gmail.com>
2020-04-24 20:41:01 +0530

    Add: Setup router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { Switch, BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/Home";
 import "./App.scss";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/home" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
