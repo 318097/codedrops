0ae0b605ac4c346919b5dc55c3f44c0a5c2638f4
Mehul Lakhanpal <318097@gmail.com>
2020-09-01 11:49:20 +0530

    Refactor: Switch config between server and functions

--- src/config.js
+++ src/config.js
@@ -1,10 +1,23 @@
+const {
+  NODE_ENV,
+  REACT_APP_SRC,
+  REACT_APP_FIREBASE_URL,
+  REACT_APP_SERVER_URL,
+  REACT_APP_COLLECTION_ID,
+} = process.env;
+
+const isProd = NODE_ENV === "production";
+
+const SERVER_URL = isProd
+  ? REACT_APP_SRC === "FIREBASE"
+    ? REACT_APP_FIREBASE_URL
+    : REACT_APP_SERVER_URL
+  : "http://localhost:7000/api";
+
 const config = {
-  SERVER_URL:
-    process.env.NODE_ENV === "production"
-      ? process.env.REACT_APP_SERVER_URL
-      : "http://localhost:7000/api",
-  COLLECTION_ID:
-    process.env.REACT_APP_COLLECTION_ID || "omway2fLvhXGnKhwtABwP4",
+  SERVER_URL,
+  COLLECTION_ID: REACT_APP_COLLECTION_ID || "omway2fLvhXGnKhwtABwP4",
+  IS_SERVER: REACT_APP_SRC !== "FIREBASE",
 };
 
 export default config;
--- src/store/posts/actions.js
+++ src/store/posts/actions.js
@@ -30,13 +30,14 @@ export const fetchTags = () => async (dispatch) => {
 export const fetchPosts = () => async (dispatch, getState) => {
   try {
     dispatch(setAppLoading(true));
+    const url = config.IS_SERVER ? `/posts` : `/getPosts`;
     const {
       posts: { filters, posts },
     } = getState();
     const updatedPosts = filters && filters.page > 1 ? [...posts] : [];
     const {
       data: { posts: data, meta },
-    } = await axios.get(`/posts?collectionId=${config.COLLECTION_ID}`, {
+    } = await axios.get(`${url}?collectionId=${config.COLLECTION_ID}`, {
       params: filters,
     });
     updatedPosts.push(...data);
@@ -67,18 +68,20 @@ export const setFilter = (filterUpdate, resetPage = true) => async (
 
 export const getPostById = (postId) => async (dispatch, getState) => {
   dispatch(setAppLoading(true));
+  const url = config.IS_SERVER ? `/posts/${postId}` : `/getPostById`;
   const {
     data: { post },
-  } = await axios.get(`/posts/${postId}?collectionId=${config.COLLECTION_ID}`);
+  } = await axios.get(`${url}?collectionId=${config.COLLECTION_ID}`);
 
   dispatch({ type: GET_POST_BY_ID, payload: post });
   dispatch(setAppLoading(false));
 };
 
 export const fetchRelatedPosts = (postId) => async (dispatch, getState) => {
+  const url = config.IS_SERVER ? `/posts/random` : `/getRandomPosts`;
   const {
     data: { posts },
-  } = await axios.get(`/posts/random?collectionId=${config.COLLECTION_ID}`);
+  } = await axios.get(`${url}?collectionId=${config.COLLECTION_ID}`);
 
   dispatch({ type: GET_RELATED_POSTS, payload: posts });
 };
