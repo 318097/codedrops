44945b715925139ac647f942ef5e9a70272e4fce
Mehul Lakhanpal <318097@gmail.com>
2020-09-03 19:47:45 +0530

    Update: Catch errors

--- src/store/posts/actions.js
+++ src/store/posts/actions.js
@@ -67,21 +67,27 @@ export const setFilter = (filterUpdate, resetPage = true) => async (
 };
 
 export const getPostById = (postId) => async (dispatch, getState) => {
-  dispatch(setAppLoading(true));
-  const url = config.IS_SERVER ? `/posts/${postId}` : `/getPostById`;
-  const {
-    data: { post },
-  } = await axios.get(`${url}?collectionId=${config.COLLECTION_ID}`);
+  try {
+    dispatch(setAppLoading(true));
+    const url = config.IS_SERVER ? `/posts/${postId}` : `/getPostById`;
+    const {
+      data: { post },
+    } = await axios.get(`${url}?collectionId=${config.COLLECTION_ID}`);
 
-  dispatch({ type: GET_POST_BY_ID, payload: post });
-  dispatch(setAppLoading(false));
+    dispatch({ type: GET_POST_BY_ID, payload: post });
+  } catch (err) {
+  } finally {
+    dispatch(setAppLoading(false));
+  }
 };
 
 export const fetchRelatedPosts = (postId) => async (dispatch, getState) => {
-  const url = config.IS_SERVER ? `/posts/random` : `/getRandomPosts`;
-  const {
-    data: { posts },
-  } = await axios.get(`${url}?collectionId=${config.COLLECTION_ID}`);
+  try {
+    const url = config.IS_SERVER ? `/posts/random` : `/getRandomPosts`;
+    const {
+      data: { posts },
+    } = await axios.get(`${url}?collectionId=${config.COLLECTION_ID}`);
 
-  dispatch({ type: GET_RELATED_POSTS, payload: posts });
+    dispatch({ type: GET_RELATED_POSTS, payload: posts });
+  } catch (err) {}
 };
