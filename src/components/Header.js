b8aae2c61430ef67dcd942cb1dc0502f3ae30b4e
Mehul Lakhanpal <318097@gmail.com>
2020-11-20 23:17:21 +0530

    Refactor

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import styled from "styled-components";
-import colors from "@codedrops/react-ui";
-
 import Filters from "./posts/Filters";
 import Logo from "../assets/logo-v3";
 
@@ -14,34 +12,14 @@ const StyledHeader = styled.header`
   justify-content: space-around;
   align-items: center;
   padding: 4px 0 6px;
-  /* background: white; */
   width: 98%;
   margin: 0 auto;
-  /* h2 {
-    font-size: 2rem;
-    color: ${colors.slate};
-    .code {
-      color: ${colors.slate};
-      font-size: 2.4rem;
-      &:after {
-        bottom: 6px;
-      }
-    }
-  }
-  @media (max-width: 480px) {
-    flex-direction: column;
-    h2 {
-      margin-right: 0px;
-    }
-  } */
 `;
 
 const Header = ({ location }) => {
   return (
     <StyledHeader>
       <Logo />
-      {/* <span className="code custom-header">Code</span>drops */}
-
       {location.pathname === "/posts" && <Filters />}
     </StyledHeader>
   );
