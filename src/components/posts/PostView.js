ef788618bb302f0b95fa1fcf3e2e868bbbffa7ec
Mehul Lakhanpal <318097@gmail.com>
2020-11-26 21:25:06 +0530

    Add: Chain view

--- src/components/posts/PostView.js
+++ src/components/posts/PostView.js
@@ -26,6 +26,42 @@ const CardWrapper = styled.div`
       padding: 20px 10px;
       font-size: 1.2rem;
       line-height: 1.6;
+      .chain-item {
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 16px;
+        .chain-item-header {
+          display: flex;
+          align-items: center;
+          margin-bottom: 8px;
+          .chain-item-id {
+            background: ${colors.strokeOne};
+            border-radius: 50%;
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
+            width: 22px;
+            height: 22px;
+            font-size: 1rem;
+            cursor: pointer;
+            transition: 0.4s;
+            margin-right: 10px;
+            position: relative;
+            top: 3px;
+          }
+          .chain-item-title {
+            position: relative;
+            top: 2px;
+            font-size: 1.2rem;
+          }
+        }
+        .chain-item-content {
+          padding-left: 12px;
+          font-size: 1.2rem;
+          line-height: 1.6;
+          color: ${colors.bar};
+        }
+      }
     }
     .tagList {
       padding-left: 4px;
@@ -57,16 +93,36 @@ const PostView = ({ history, match, post, getPostById, tagColors }) => {
 
   if (!post) return null;
 
-  const { title, content, tags = [], _id } = post || {};
+  const { title, content, tags = [], _id, type, chainedPosts = [] } =
+    post || {};
   return (
     <section id="view-post">
       <CardWrapper className="post-wrapper">
         <Card bottomLine>
           <h3 className="title">{title}</h3>
-          <div
-            className="content"
-            dangerouslySetInnerHTML={{ __html: marked(content) }}
-          ></div>
+          {type === "CHAIN" ? (
+            <div className="content">
+              {chainedPosts.map((post, index) => (
+                <div className="chain-item" key={post._id}>
+                  <div className="chain-item-header">
+                    <div className="chain-item-id">{index + 1}</div>
+                    <h3 className="chain-item-title">{post.title}</h3>
+                  </div>
+                  <div
+                    className="chain-item-content"
+                    dangerouslySetInnerHTML={{
+                      __html: marked(post.content || ""),
+                    }}
+                  />
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div
+              className="content"
+              dangerouslySetInnerHTML={{ __html: marked(content) }}
+            />
+          )}
           <div className="tagList">
             {tags.map((tag, index) => (
               <Tag
